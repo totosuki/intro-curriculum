cf748328c1ef11b42552be5c5c45b98205f14b67
moesuke <81308242+moesuke@users.noreply.github.com>
2022-07-25 09:05:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const server = http.createServer((req, res) => {
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info(`Listening on ${port}`);
+}); 
 });
