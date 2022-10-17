c1ac09c01644fc7b6038d4564b388b33ec7def1b
oosakiken1 <9690408@gmail.com>
2022-10-17 15:02:14 +0900

    教材との差分を修正

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  res.setHeader('Set-Cookie', `last_access=${now};`);
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
   console.info(`Listening on ${port}`);
-}); 
+});
