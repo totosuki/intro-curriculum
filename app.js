644c4823441b95340f249e8670832e08668db78b
totosuki <e231kari@gmail.com>
2023-10-08 20:36:57 +0900

    疑似catコマンドを実装

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 const fs = require('node:fs');
 
-function cat(fileName) {
-  const content = fs.readFile(fileName, 'utf8', () => {});
+async function cat(fileName) {
+  const content = await fs.promises.readFile(fileName, 'utf8', () => {});
   console.log(content);
 }
 
