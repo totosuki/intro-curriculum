e7e1b9a505f6ca71d2f8ebd47c6d2630c892bf75
totosuki <e231kari@gmail.com>
2023-10-10 18:29:47 +0900

    ファイルを削除してからテストを実行するように修正

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('node:assert');
+
+const fs = require("node:fs");
+fs.unlinkSync("./tasks.json");
+const todo = require("./index.js");
 
 // add と list のテスト
 todo.add('ノートを買う');
