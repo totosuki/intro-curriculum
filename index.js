f6ecfb4d6d9e9f584a5f54aa5261408c27438486
totosuki <e231kari@gmail.com>
2023-10-11 14:48:59 +0900

    有効期限を今日の30日後に設定

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
+  const expiresDate = new Date(now + 30 * 86400 + 1000);
   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.setHeader('Set-Cookie', `last_access=${now};`);
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${expiresDate.toUTCString()}`);
   const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toString());
 });
