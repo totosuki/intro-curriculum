c7fd1607c3d3a21fd16243848d6bf4c66b0477ad
totosuki <e231kari@gmail.com>
2023-10-11 11:22:51 +0900

    名前入力欄を実装し、ログを見やすくした

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const answer = new URLSearchParams(decoded)
+            console.info(`[${now}] ${answer.get("name")}さんは${answer.get("yaki-tofu")}に投票しました`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${answer.get("name")}さんは${answer.get("yaki-tofu")}に投票しました</h1></body></html>`
             );
             res.end();
           });
