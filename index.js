68dd3751941b39ac37a7b9236fb37b50d52409ca
totosuki <e231kari@gmail.com>
2023-10-11 12:19:27 +0900

    3つめのアンケートを追加

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const server = http
             firstItem: 'ごはん',
             secondItem: 'パン'
           }));
+        } else if (req.url === "/enquetes/sushi-pizza") {
+          res.write(pug.renderFile("./form.pug", {
+            path: req.url,
+            firstItem: "寿司",
+            secondItem: "ピザ"
+          }));
         }
         res.end();
         break;
